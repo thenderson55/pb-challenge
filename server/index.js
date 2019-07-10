dd0e2838a2b6f243234525044d37e9697f685151
Thomas Henderson <tbhenderson55@yahoo.com>
2019-07-10 10:00:28 +0900

    pointed server to build folder

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(express.static('build'));
+
+app.get("/api", function(req, res) {
   res.send({
     message: "PlayBrain desu"
   })
 })
+
+app.get('*', (req, res) => {
+  app.use(express.static('build'));
+});
 
 app.listen(PORT, function(){
     console.log(`Your node js server is running on port ${PORT}`);
