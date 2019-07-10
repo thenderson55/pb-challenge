27226f9f6931607298ce94499bddbb4062950341
Thomas Henderson <tbhenderson55@yahoo.com>
2019-07-10 20:38:19 +0900

    border color changes selectively

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,17 +1,15 @@
 import React, { useContext, useState } from "react";
-// import Player from "./Player";
 import styled from "styled-components";
 import playersContext from "../context/playersContext";
 import wales from "../images/wales.png"
 
 const PlayerList = (props) => {
   const { players } = useContext(playersContext);
-  // console.log(region, players)
-  // let playerList;
+
   const [user, setUser] = useState({ id: 1, admin: false, votes: []
   })
   const [voteCount, setVoteCount] = useState(0)
-  console.log(user)
+  const [votedList, setVotedList] = useState([])
 
   const Player = styled.li`
     width: 170px;
@@ -27,13 +25,16 @@ const PlayerList = (props) => {
     overflow: hidden;
     border-width: 5px;
     border-style: solid;
-
     border-color: rgb(71, 81, 93);
     border-image: initial;
     border-radius: 50%;
     &:hover {
       border-color: rgb(255, 125, 8);
     }
+  `;
+
+  const VotedAvatar = styled(PlayerAvatar)`
+    border-color: rgb(255, 125, 8);
   `;
 
   const PlayerName = styled.p`
@@ -55,13 +56,17 @@ const PlayerList = (props) => {
     /* justify-content: center; */
   `;
 
-  const selectPlayer = (nickname, country) => {
+  const selectPlayer = (nickname, country, index) => {
     console.log(nickname)
     if(user.votes.some(player => player.nickname == nickname)){
       const checkToRemove = window.confirm('You have already voted for this player, do you want to remove him/her from your voting selection?')
       if(checkToRemove){
         user.votes.splice(user.votes.findIndex(player => player.nickname == nickname),1);
+
         setVoteCount(user.votes.length)
+
+        const newVotedList = votedList.filter(item => item !== index)
+        setVotedList(newVotedList)
         return
       }
       return
@@ -73,12 +78,14 @@ const PlayerList = (props) => {
     if(user.votes.length == 3 ){
       alert("You have already voted for three people")
       return;
-    }   
+    } 
+
     user.votes.push({nickname: nickname, country: country})
     setVoteCount(user.votes.length)
-    console.log(user)
+    setVotedList([...votedList, index])
   }
   
+
   const playerList = (
     <>
     <p>You have voted {voteCount} times.</p>
@@ -86,13 +93,24 @@ const PlayerList = (props) => {
       {players &&
         players.map((player, i) => {
           if (player.country == props.region) {
-            return (
-              <Player onClick={() => selectPlayer(player.nickname, player.country)} key={i}>
-                <PlayerAvatar src={player.avatar}/>
-                <PlayerName>{player.nickname} <img src={wales} alt=""/></PlayerName>
-                <PlayerMessage>{player.message}</PlayerMessage>
-              </Player>
-            );
+            if(votedList.includes(i)){
+              return (  
+                <Player onClick={() => selectPlayer(player.nickname, player.country, i)} key={i}>
+                  <VotedAvatar src={player.avatar}/>
+                  <PlayerName>{player.nickname} <img src={wales} alt=""/></PlayerName>
+                  <PlayerMessage>{player.message}</PlayerMessage>
+                </Player>
+              );
+            } 
+            else {
+              return (
+                <Player onClick={() => selectPlayer(player.nickname, player.country, i)} key={i}>
+                  <PlayerAvatar src={player.avatar}/>
+                  <PlayerName>{player.nickname} <img src={wales} alt=""/></PlayerName>
+                  <PlayerMessage>{player.message}</PlayerMessage>
+                </Player>
+              );
+            }
           }
         })}
     </PlayerWrapper>
